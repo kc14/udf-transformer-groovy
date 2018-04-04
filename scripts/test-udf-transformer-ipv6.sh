b3ba5f14607af06f85be14fc91400a1565030311
Frank Kemmer <frank.kemmer@gmail.com>
2018-04-04 16:51:07 +0200

    Moved concat to UDF package Hive

--- scripts/test-udf-transformer-ipv6.sh
+++ scripts/test-udf-transformer-ipv6.sh
@@ -2,6 +2,6 @@
 
 echo $'a\tb\tc\t127.126.125.124\t223.123.124.129\tf\tg\th\ti\tj\tk\tl\tm\tn\to\tp\tq\tr' | \
 java -Xmx4G -cp "${BASEDIR}/target/udf-transformer-groovy-0.0.1-SNAPSHOT-jar-with-dependencies.jar" \
-Use Common,IPv6 \
+Use Hive,IPv6 \
 --ipv6-ranges "${BASEDIR}/resources/ipv6+4_network_ranges.gz" \
---select "[ c[0], c[1], c[2], c[3], c[4], ipv6.ip(c[3]), ipv6.ip(c[4]), c[5], c[6], c[7], c[8], c[9], c[10], c[11], c[12], c[13], c[14], c[15], c[16], common.concat(c[0], c[1]), common.concat_v(c[5], c[6], c[7]) ]"
+--select "[ c[0], c[1], c[2], c[3], c[4], ipv6.ip(c[3]), ipv6.ip(c[4]), c[5], c[6], c[7], c[8], c[9], c[10], c[11], c[12], c[13], c[14], c[15], c[16], hive.concat(c[0], c[1]), hive.concat(c[5], c[6], c[7]) ]"
